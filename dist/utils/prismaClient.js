73662000b25a4fbb981a54af9ba94112137704b2
milki <marko.milkovicc@gmail.com>
2026-03-30 19:47:26 +0200

    Added new prisma configs

--- dist/utils/prismaClient.js
+++ dist/utils/prismaClient.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const client_1 = require("../../generated/prisma/client");
+const client_1 = require("@prisma/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 const pg_1 = __importDefault(require("pg"));
 const pool = new pg_1.default.Pool({
